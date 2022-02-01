1b0d4346733a2973737f9dffac1536299759222d
victorlauria <victor.lauria@atlanticsolutions.com.br>
2022-02-01 09:32:35 -0300

    Added updates in DemoResources

--- src/main/java/com/example/demo/resources/DemoResource.java
+++ src/main/java/com/example/demo/resources/DemoResource.java
@@ -10,32 +10,42 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/demo")
 public class DemoResource {
 
-//	private final String TELA_DEMO = "demo";
+	private final String TELA_DEMO = "demo";
+	private final String TELA_PAGINA_TESTE = "pagina-teste";
+	private final String TELA_TESTE = "teste";
+	private final String TELA_NEW_FILE = "NewFile";
 	
 	@RequestMapping(value = "/ola", method = RequestMethod.GET)
 	public String ola(ModelMap modelMap) {
 		modelMap.addAttribute("atributo", "Thymeleaf");
-		return "demo";
+		return TELA_DEMO;
 	}
 	
 	@RequestMapping(value = "/demo",  method = RequestMethod.GET)
 	public ModelAndView paginaDemo() {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("demo");
+		modelAndView.setViewName(TELA_DEMO);
 		return modelAndView;
 	}
 	
 	@RequestMapping(value = "/pagina-teste",  method = RequestMethod.GET)
 	public ModelAndView paginaTeste() {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("pagina-teste");
+		modelAndView.setViewName(TELA_PAGINA_TESTE);
 		return modelAndView;
 	}
 	
 	@RequestMapping(value = "/teste",  method = RequestMethod.GET)
 	public ModelAndView teste() {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("teste");
+		modelAndView.setViewName(TELA_TESTE);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/new-file", method = RequestMethod.GET)
+	public ModelAndView newFile() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName(TELA_NEW_FILE);
 		return modelAndView;
 	}
 }
